f0fbcd83a3056f94dcfdaf391ca4d2edc01665c4
amits-77 <amitbajrangisahu2002@gmail.com>
2022-11-06 20:05:41 +0530

    First Commit
    Uploading All files

---
+++ docker-jenkins/src/test/java/GroupId/docker_jenkins/dockerTest.java
@@ -0,0 +1,10 @@
+package GroupId.docker_jenkins;
+
+public class dockerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!"); 
+	}
+
+}
